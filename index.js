25a5bd6b4924407d8a0731f5e33c93bb67f6288b
Max Arouca <maximiler@gmail.com>
2018-10-25 20:19:51 -0300

    Install ESLint and format code

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-import { ApolloServer, gql } from 'apollo-server';
-import { makeExecutableSchema } from 'graphql-tools';
+import { ApolloServer, gql } from 'apollo-server'
+import { makeExecutableSchema } from 'graphql-tools'
+import * as BookType from './src/modules/book/BookType'
+import * as AuthorType from './src/modules/author/AuthorType'
+import Books from './src/modules/book/BookModel'
+import Author from './src/modules/author/authorModel'
 import mongoose from 'mongoose'
 const mongo = 'mongodb://apollo:apollo123@ds215822.mlab.com:15822/apolloserver'
-
-import * as BookType from './src/modules/book/BookType';
-import * as AuthorType from './src/modules/author/AuthorType';
-import Books from "./src/modules/book/BookModel";
-import Author from "./src/modules/author/authorModel";
 
 const SchemaDefinition = gql`
   schema {
@@ -22,15 +21,12 @@ const SchemaDefinition = gql`
   type Mutation {
     addBook(title: String!, author: AuthorInput!): Book
     addAuthor(name: String!, age: Int!): Author
-    removeBook(title:String!): [Book]
-    removeAuthor(name:String!): [Author]
+    removeBook(title: String!): [Book]
+    removeAuthor(name: String!): [Author]
   }
-`;
+`
 
-const typeDefs = [
-  BookType.typeDefs,
-  AuthorType.typeDefs,
-]
+const typeDefs = [BookType.typeDefs, AuthorType.typeDefs]
 
 const resolvers = {
   Query: {
@@ -39,35 +35,39 @@ const resolvers = {
   },
   Mutation: {
     addBook: (root, args, { Books }) => {
-
       Author.findOne({ name: args.author.name }, (err, author) => {
         if (!author) {
-          const author = new Author(args.author);
-          author.save();
-          return author;
+          const author = new Author(args.author)
+          author.save()
+          return author
+        }
+        if (err) {
+          console.log(err)
         }
       })
 
-      const book = new Books(args);
-      book.save();
-      return book;
+      const book = new Books(args)
+      book.save()
+      return book
     },
     addAuthor: (root, args, { Author }) => {
-      const author = new Author(args);
-      author.save();
-      return author;
+      const author = new Author(args)
+      author.save()
+      return author
     },
-    removeBook: (root, args, {Books}) => {
-      Books.deleteOne(args, function (err) { console.log(err)});
+    removeBook: (root, args, { Books }) => {
+      Books.deleteOne(args, function (err) {
+        console.log(err)
+      })
     },
-    removeAuthor: (root, args, {Author}) => {
-      console.log(args);
-      Author.deleteOne(args, function(err) {
-        console.log(err);
-      });
+    removeAuthor: (root, args, { Author }) => {
+      console.log(args)
+      Author.deleteOne(args, function (err) {
+        console.log(err)
+      })
     }
   }
-};
+}
 
 const schema = makeExecutableSchema({
   typeDefs: [SchemaDefinition, ...typeDefs],
@@ -75,10 +75,12 @@ const schema = makeExecutableSchema({
 })
 
 const server = new ApolloServer({
-  schema, context: ({ req }) => ({
-    Books, Author
+  schema,
+  context: ({ req }) => ({
+    Books,
+    Author
   })
-});
+})
 
 mongoose
   .connect(
@@ -87,7 +89,7 @@ mongoose
   )
   .then(() => {
     server.listen().then(({ url }) => {
-      console.log(`🚀  Server ready at ${url}`);
-    });
+      console.log(`🚀  Server ready at ${url}`)
+    })
   })
-  .catch(e => console.log(e));
+  .catch(e => console.log(e))
